b28e48739410380b2102d69d4243d6dafcb18a06
Shin Pham <phamconghau012@gmail.com>
2025-05-22 23:48:31 +0700

    add file

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,10 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RevenueController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-use App\Http\Controllers\ProductReviewController;
 
-Route::get('/product-reviews', [ProductReviewController::class, 'index'])->name('product_reviews.index');
-Route::get('/product-reviews/create', [ProductReviewController::class, 'create'])->name('product_reviews.create');
-Route::post('/product-reviews', [ProductReviewController::class, 'store'])->name('product_reviews.store');
-use App\Http\Controllers\RemoveFavoriteController;
-
-Route::get('/remove-favorites', [RemoveFavoriteController::class, 'index'])->name('remove_favorites.index');
-Route::get('/remove-favorites/create', [RemoveFavoriteController::class, 'create'])->name('remove_favorites.create');
-Route::post('/remove-favorites', [RemoveFavoriteController::class, 'store'])->name('remove_favorites.store');
+Route::get('/revenues', [RevenueController::class, 'index'])->name('revenues.index');
+Route::get('/revenues/create', [RevenueController::class, 'create'])->name('revenues.create');
+Route::post('/revenues', [RevenueController::class, 'store'])->name('revenues.store');
