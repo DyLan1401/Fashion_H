7f74abf7386ed8713a272c6e9e37e7850b815161
Shin Pham <phamconghau012@gmail.com>
2025-05-23 00:14:34 +0700

    add file

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+use App\Http\Controllers\ProductReviewController;
 
-use App\Http\Controllers\HomeController;
+Route::get('/product-reviews', [ProductReviewController::class, 'index'])->name('product_reviews.index');
+Route::get('/product-reviews/create', [ProductReviewController::class, 'create'])->name('product_reviews.create');
+Route::post('/product-reviews', [ProductReviewController::class, 'store'])->name('product_reviews.store');
+use App\Http\Controllers\RemoveFavoriteController;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/remove-favorites', [RemoveFavoriteController::class, 'index'])->name('remove_favorites.index');
+Route::get('/remove-favorites/create', [RemoveFavoriteController::class, 'create'])->name('remove_favorites.create');
+Route::post('/remove-favorites', [RemoveFavoriteController::class, 'store'])->name('remove_favorites.store');
